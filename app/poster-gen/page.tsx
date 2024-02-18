fed591209c84f08f773db5e33377be3c11b2389c
wilfredeveloper <pxumtech@gmail.com>
2024-02-19 01:29:38 +0300

    Add PosterGenPage component and import MultiStepForm

---
+++ app/poster-gen/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import MultiStepForm from "@/components/MultiStepForm/MultiStepForm";
+
+export default function PosterGenPage() {
+  return (
+    <main>
+        <MultiStepForm />
+    </main>
+  );
+}
